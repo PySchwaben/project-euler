c076878240f3293c3536de0a16beb32090fc222e
Oliver Bestwalter <oliver@bestwalter.de>
2014-08-18 23:14:43 +0200

    Erstes Problem

---
+++ e1.py
@@ -0,0 +1,15 @@
+"""
+Multiples of 3 and 5
+====================
+
+If we list all the natural numbers below 10 that are multiples of
+3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+https://projecteuler.net/problem=1
+"""
+
+
+is_multiple_of_3_or_5 = lambda n: n % 3 == 0 or n % 5 == 0
+print(sum([n for n in range(1, 1000) if is_multiple_of_3_or_5(n)]))
